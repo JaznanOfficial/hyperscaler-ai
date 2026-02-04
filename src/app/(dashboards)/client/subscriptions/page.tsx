9d3b0e000c411e96925715d4e71dce80c1ae954e
JaznanOfficial <jaznanofficial@gmail.com>
2026-02-04 19:30:52 +0600

    tab trigger mobile responsiveness issue fix

--- src/app/(dashboards)/client/subscriptions/page.tsx
+++ src/app/(dashboards)/client/subscriptions/page.tsx
@@ -29,22 +29,22 @@ export default function ClientSubscriptionsPage() {
             </p>
           </div>
           <Tabs defaultValue="all" className="w-full mt-8">
-            <TabsList className="rounded-full px-2 py-2 text-base h-12! bg-white border border-slate-300 cursor-pointer">
-              <TabsTrigger value="all" className="rounded-full px-5 py-2 text-lg h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
+            <TabsList className="rounded-full lg:px-2 lg:py-2 text-base lg:h-12! bg-white border border-slate-300 cursor-pointer">
+              <TabsTrigger value="all" className="rounded-full lg:px-5 lg:py-2 lg:text-lg lg:h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
                 <span>All</span>
-                <span className="ml-2 flex h-6 w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
+                <span className="ml-2 flex h-4 lg:h-6 w-4 lg:w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
                   5
                 </span>
               </TabsTrigger>
-              <TabsTrigger value="active" className="rounded-full px-5 py-2 text-lg h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
+              <TabsTrigger value="active" className="rounded-full lg:px-5 lg:py-2 lg:text-lg lg:h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
                 <span>Active</span>
-                <span className="ml-2 flex h-6 w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
+                <span className="ml-2 flex h-4 lg:h-6 w-4 lg:w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
                   1
                 </span>
               </TabsTrigger>
-              <TabsTrigger value="cancelled" className="rounded-full px-5 py-2 text-lg h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
+              <TabsTrigger value="cancelled" className="rounded-full lg:px-5 lg:py-2 lg:text-lg lg:h-9 data-[state=active]:bg-[#9E32DD] data-[state=active]:text-white cursor-pointer">
                 <span>Cancelled</span>
-                <span className="ml-2 flex h-6 w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
+                <span className="ml-2 flex h-4 lg:h-6 w-4 lg:w-6 items-center justify-center rounded-full bg-fuchsia-100 text-sm font-semibold text-fuchsia-700">
                   1
                 </span>
               </TabsTrigger>
