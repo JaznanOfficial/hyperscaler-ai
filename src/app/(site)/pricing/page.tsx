5e9cd94095cd4b4aabade666bfe4001071726c5c
JaznanOfficial <jaznanofficial@gmail.com>
2026-03-09 14:15:10 +0600

    create new pricing page

---
+++ src/app/(site)/pricing/page.tsx
@@ -0,0 +1,9 @@
+import Pricing from "@/components/home/pricing/pricing";
+
+export default function PricingPage() {
+  return (
+    <main className="bg-white">
+      <Pricing />
+    </main>
+  );
+}
